073fed6dd10395713ce1363c96c4bc4dd2b02608
p.sereda <piotr.sereda@dentsu.com>
2023-05-17 12:37:30 +0200

    featuer/issue-6 add hamburger and  menu on mobile devices

---
+++ web/themes/custom/lesnik/js/menu.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {  
+        $( ".cross" ).hide();
+        $( ".hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "fast", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
+        });
+        });
+        
+        $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "fast", function() {
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
+        });
+        });
+
+    });
